3ea75364bfed82bff9957578ea71809fbbe69c2f
KrishJakhar <157725072+krish10007@users.noreply.github.com>
2026-04-07 10:03:19 -0400

    Add: car-fleet - submission-15

---
+++ Data Structures & Algorithms/car-fleet/submission-15.py
@@ -0,0 +1,11 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        posSpeed = [(p,s) for p,s in zip(position,speed)]
+        stk = []
+        for p,s in sorted(posSpeed)[::-1]:
+            time = (target - p)/s
+            if not stk or stk[-1] < time:
+                stk.append(time)
+        return len(stk)
+
+
