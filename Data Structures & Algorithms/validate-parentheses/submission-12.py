fd1cc687be0d9d8435a80963b64b3151564dffb0
KrishJakhar <157725072+krish10007@users.noreply.github.com>
2026-04-07 08:36:55 -0400

    Add: validate-parentheses - submission-12

---
+++ Data Structures & Algorithms/validate-parentheses/submission-12.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        map = {')':'(', ']':'[', '}':'{'}
+        stk = []
+
+        for x in s:
+            if x not in map:
+                stk.append(x)
+            else:
+                if not stk:
+                    return False
+                popped = stk.pop()
+                if map[x] != popped:
+                    return False
+        return not stk
+
+#time - O(n)
+#space - O(n) in worst case if all elements are added to stack
