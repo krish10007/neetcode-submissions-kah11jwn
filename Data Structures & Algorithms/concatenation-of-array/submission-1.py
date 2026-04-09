946cf2c25476f55cb7b4d0a4c79221f91c7ad311
KrishJakhar <157725072+krish10007@users.noreply.github.com>
2026-04-09 17:16:31 -0400

    Add: concatenation-of-array - submission-1

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-1.py
@@ -0,0 +1,6 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        return nums + nums
+
+# Time complexity is O(n) as Copying elements takes linear time
+# Space complexity is O(n) because we create a new array of size 2n.
