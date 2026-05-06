bd85041bac507051ab9bc2fc888c96e2ef86514b
KrishJakhar <157725072+krish10007@users.noreply.github.com>
2026-05-05 20:11:31 -0400

    Add: longest-common-prefix - submission-14

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-14.py
@@ -0,0 +1,20 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        minlength = float('inf')
+
+        for s in strs:
+            if len(s) < minlength:
+                minlength = len(s)
+        
+        i = 0
+        while i < minlength:
+            for s in strs:
+                if s[i] != strs[0][i]:
+                    return s[:i]
+            i+=1
+        return s[:i] # or strs[0][:i]
+
+# Time complexity is O(n × m), where n is the number of strings 
+# and m is the length of the shortest string, 
+# since we compare each character across all strings.
+# Space complexity is O(1) excluding the output.
