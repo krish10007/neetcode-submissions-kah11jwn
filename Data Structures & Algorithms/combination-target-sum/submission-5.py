ab105f6625ccf34ff8b8463431a98f2b421b02e4
KrishJakhar <157725072+krish10007@users.noreply.github.com>
2026-08-17 11:02:22 -0400

    Add: combination-target-sum - submission-5

---
+++ Data Structures & Algorithms/combination-target-sum/submission-5.py
@@ -0,0 +1,23 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        res = []
+
+        def backtrack(i,sol):
+            if sum(sol) == target:
+                res.append(sol[:])
+                return 
+            
+            if i == len(nums) or sum(sol) > target:
+                return
+            
+            sol.append(nums[i])
+            backtrack(i,sol)
+
+            sol.pop()
+            backtrack(i+1, sol)
+        
+        backtrack(0,[])
+        return res
+
+#sum recalculates the entire list every recursion so the version of sol1
+#with total is better
